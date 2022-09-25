c0fa50671d92a7b1dce014b01b371c5bb3ab1922
Samyuktha18 <samyuktha020918@gmail.com>
2022-09-25 20:33:12 +0530

    Build: Note saving

---
+++ main.py
@@ -0,0 +1,6 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__': #created in __init__.py
+    app.run(debug=True) #automatically rerun the web server if we have changed the code
